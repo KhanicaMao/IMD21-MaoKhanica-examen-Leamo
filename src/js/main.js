4b9c419dfcb89d7404c0589b40ef9f7b3cc33385
KhanicaMao <khanica.mao@studentfr.ch>
2023-05-17 11:17:34 +0200

    Affiner l'accueil et ajouter le carousel

--- src/js/main.js
+++ src/js/main.js
@@ -12,3 +12,17 @@ function openNav() {
 function closenNav() {
   sidenav.classList.remove("active");
 }
+
+import Swiper, { Navigation, Pagination } from "swiper";
+
+Swiper.use([Navigation, Pagination]);
+
+const swiper = new Swiper(".swiper-container", {
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
